395d4cbc7d8b7c416d81c8cf087f8a3d8d0576e6
GouzzzGouzzz <110063120+GouzzzGouzzz@users.noreply.github.com>
2023-11-28 16:16:37 +0100

    idk

--- srcs/requirements/wordpress/wp_init.sh
+++ srcs/requirements/wordpress/wp_init.sh
@@ -1,5 +1,8 @@
 sleep 10;
 cd /var/www/html/ && wp config create --allow-root --dbname=$DATABASE --dbuser=$USER --dbpass=$PASSWORD --dbhost=mariadb:3306 --path='/var/www/html'
+
 cd /var/www/html/ && wp core install --allow-root --url=$URL --title=$TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$MAIL --skip-email
-cd /var/www/html/ && wp user create --allow-root $USER_WP $USER_WP_MAIL --user_pass=$USER_WP_PASS
+
+wp theme activate twentytwentyfour --allow-root
+
 exec php-fpm8.2 -F
